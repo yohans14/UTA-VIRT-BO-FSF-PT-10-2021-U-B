f6525db27bc6fd397dd491bda428688551ff5ccd
Bob Deckard <bdeckard92@gmail.com>
2022-01-12 10:56:56 -0600

    adds alternative algo solutions

--- 10-OOP/01-Activities/01-TDD/Solved/test/algo.test.js
+++ 10-OOP/01-Activities/01-TDD/Solved/test/algo.test.js
@@ -1,23 +1,18 @@
-// Require algo file
 const Algo = require('../algo');
 
 describe('Algo', () => {
   describe('reverse', () => {
-    // 'reverse' should reverse "Hello World!"
     it('should reverse a given string', () => {
-      // In order to test the function, we need to create a working example. First we define 'str'.
       const str = 'Hello World!';
-      // Next we define 'reversed' as the value of 'str' reversed.
       const reversed = '!dlroW olleH';
-      // We pass 'str' into '.reverse()' and set that value equal to result
+
       const result = new Algo().reverse(str);
-      // We expect 'result' to equal 'reversed' if the 'reverse()' function is working correctly.
+
       expect(result).toEqual(reversed);
     });
   });
 
   describe('isPalindrome', () => {
-    // First test for '.isPalindrome()'
     it('should return true if a string is a palindrome', () => {
       const str = 'racecar';
 
@@ -25,7 +20,7 @@ describe('Algo', () => {
 
       expect(result).toEqual(true);
     });
-    // Second test for '.isPalindrome()'
+
     it('should return false if a string is not a palindrome', () => {
       const str = 'neon';
 
